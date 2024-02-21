304c60daf5001adb25117be7cc0e8548e2b2465e
tisha <156155063+tishaProjek@users.noreply.github.com>
2024-02-21 10:25:09 +0700

    Add files via upload

---
+++ Phone.java
@@ -0,0 +1,11 @@
+package com.mycompany.phone;
+
+public interface Phone {
+    int MAX_VOLUME = 100;
+    int MIN_VOLUME = 0;
+    
+    void powerOn();
+    void powerOff();
+    void volumeUp();
+    void volumeDown();
+}    
---
+++ PhoneUser.java
@@ -0,0 +1,27 @@
+package com.mycompany.phone;
+
+public class PhoneUser {
+    
+     private Phone phone;
+
+    public PhoneUser(Phone phone) {
+        this.phone = phone;
+    }
+    
+    void turnOnThePhone(){
+        this.phone.powerOn();
+    }
+    
+    void turnOffThePhone(){
+        this.phone.powerOff();
+    }
+    
+    void makePhoneLouder(){
+        this.phone.volumeUp();
+    }
+    
+    void makePhoneSilent(){
+        this.phone.volumeDown();
+    }
+    
+}
